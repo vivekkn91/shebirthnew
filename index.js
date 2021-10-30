b53c25cdc409337921154e1260c3113a5c6210ad
vivek kn <vivekkn91@gmail.com>
2021-10-30 19:31:06 +0530

    heroku7

--- index.js
+++ index.js
@@ -46,32 +46,32 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
   //   if (err) throw err;
   //   console.log("Data created!");
 
-  app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://ask-over.netlify.app"
-    );
+  // app.use(function (req, res, next) {
+  //   // Website you wish to allow to connect
+  //   res.setHeader(
+  //     "Access-Control-Allow-Origin",
+  //     "https://ask-over.netlify.app"
+  //   );
 
-    // Request methods you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
+  //   // Request methods you wish to allow
+  //   res.setHeader(
+  //     "Access-Control-Allow-Methods",
+  //     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  //   );
 
-    // Request headers you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
+  //   // Request headers you wish to allow
+  //   res.setHeader(
+  //     "Access-Control-Allow-Headers",
+  //     "X-Requested-With,content-type"
+  //   );
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
+  //   // Set to true if you need the website to include cookies in the requests sent
+  //   // to the API (e.g. in case you use sessions)
+  //   res.setHeader("Access-Control-Allow-Credentials", true);
 
-    // Pass to next layer of middleware
-    next();
-  });
+  //   // Pass to next layer of middleware
+  //   next();
+  // });
   app.use(express.urlencoded({ extended: true }));
   app.post("/questionpost", function (req, res) {
     let formData = req.body;
